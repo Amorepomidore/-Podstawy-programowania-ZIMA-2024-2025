d5aee3f6b1ce0652b81bcb13bc2fcf560df729f7
Amorepomidore <kamilbartosz16@gmail.com>
2024-11-01 21:22:01 +0100

    😂

---
+++ TypesAndVariables/Variables/1.py
@@ -0,0 +1,17 @@
+company = "ABC Data"
+phone = "555-123-009"
+employees = 25
+remote_work = True
+big_company = employees > 100
+income = 4500000
+income_per_person = income / employees
+
+print("company", company, type(company));
+print("phone", phone, type(phone));
+print("employees", employees, type(employees));
+print("remote_work", remote_work, type(remote_work));
+print("big_company", big_company, type(big_company));
+print("income", income, type(income));
+print("income_per_person", income_per_person, type(income_per_person));
+
+# 😎
---
+++ TypesAndVariables/Variables/2.py
@@ -0,0 +1,22 @@
+###
+# A program that calculates the sum of two numbers.
+# Modify the program to calculate the sum of three numbers.
+#
+number1 = 71
+number2 = 14
+result = number1 + number2
+print('Number 1: ', number1)
+print('Number 2: ', number2)
+print('The result of summation: ', result)
+
+
+# 😎
+number1 = 71
+number2 = 14
+# dodaje 3 
+number3 = 3
+result = number1 + number2 + number3
+print('Number 1: ', number1)
+print('Number 2: ', number2)
+print('Number 3: ', number3)
+print('The result of summation: ', result)
