74cb092c869c0334326b424872e50ab0067ea312
Amorepomidore <kamilbartosz16@gmail.com>
2024-11-01 20:33:02 +0100

    :D

---
+++ TypesAndVariables/Interactive and Script Modes/2.py
@@ -0,0 +1,39 @@
+# i
+print(7 + 4);
+# ii
+print((30 + 10) / 2);
+# iii
+print(2 ** 3);
+# iv
+print(7 > 5);
+# v
+r = 4;
+# vi
+area = 3.14159 * r * r;
+# vii
+print(area);
+# viii
+print(round(area, 2));
+# ix
+first_name = "Mary";
+print(first_name);
+# x
+last_name = "Brown";
+print(last_name);
+# xi
+full_name = first_name + " " + last_name;
+# xii
+print(full_name);
+# xiii
+print(len(full_name));
+# xiv
+a = 5;
+h = 4;
+areaTriangle = (a * h / 2);
+print(areaTriangle);
+# xv
+number1 = 7;
+number2 = 12;
+number3 = 31;
+srednia = (number1 + number2 + number3) / 3;
+print(round(srednia, 2));
