5a05f5529c83bc31504d1c5af1e718f2699e2241
Amorepomidore <kamilbartosz16@gmail.com>
2024-11-02 17:18:38 +0100

    🤓

---
+++ TypesAndVariables/Output Formatting/1.py
@@ -0,0 +1,11 @@
+###
+# Printing student's personal data
+#
+name = "Kamil";
+age = 20;
+height = 1.75;
+
+
+print(f"My name is {name}. I am {age} years old. I am {height} cm. In 6 years I will be {age + 6} years old.");
+
+# 😎
